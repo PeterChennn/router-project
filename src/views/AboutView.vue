c569a5022e4fc18432313a4843bb0f1698df470c
Peter <tp6u035@gmail.com>
2022-03-19 15:50:00 +0800

    initial commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam veritatis quasi voluptatum distinctio, amet alias soluta quisquam porro, eum deleniti nulla ipsam provident repudiandae nisi eveniet quod maiores aut cum corrupti illum rerum exercitationem, natus aliquid! Vero minima rerum sed voluptas quasi culpa autem odit, sequi officia enim. Accusamus, doloremque.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam veritatis quasi voluptatum distinctio, amet alias soluta quisquam porro, eum deleniti nulla ipsam provident repudiandae nisi eveniet quod maiores aut cum corrupti illum rerum exercitationem, natus aliquid! Vero minima rerum sed voluptas quasi culpa autem odit, sequi officia enim. Accusamus, doloremque.</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis corporis, illo possimus mollitia expedita autem neque, asperiores cupiditate quo atque vitae eveniet architecto quam officia et. Delectus earum ipsa consectetur nobis atque quibusdam voluptate inventore assumenda quas, iusto ad modi dicta magni, iste quaerat natus magnam minus quam porro sequi numquam deleniti! Illo modi libero laudantium eius, repellendus natus, unde nesciunt delectus error facilis ipsam dignissimos debitis reprehenderit culpa repellat voluptatum? Porro soluta, quibusdam fugit similique pariatur culpa facere asperiores rem obcaecati illum quis explicabo eveniet atque, eos animi aspernatur error itaque? Suscipit necessitatibus laudantium aspernatur error possimus quam tempora.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis corporis, illo possimus mollitia expedita autem neque, asperiores cupiditate quo atque vitae eveniet architecto quam officia et. Delectus earum ipsa consectetur nobis atque quibusdam voluptate inventore assumenda quas, iusto ad modi dicta magni, iste quaerat natus magnam minus quam porro sequi numquam deleniti! Illo modi libero laudantium eius, repellendus natus, unde nesciunt delectus error facilis ipsam dignissimos debitis reprehenderit culpa repellat voluptatum? Porro soluta, quibusdam fugit similique pariatur culpa facere asperiores rem obcaecati illum quis explicabo eveniet atque, eos animi aspernatur error itaque? Suscipit necessitatibus laudantium aspernatur error possimus quam tempora.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
